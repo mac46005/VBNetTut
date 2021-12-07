4ece6fcd5a07525149d571e5434b3639be11c4b3
Marco Preciado <mac46005@gmail.com>
2021-12-07 16:27:44 -0600

    Class example

---
+++ VBTutConsole/Dog.vb
@@ -0,0 +1,8 @@
+﻿Public Class Dog
+    Private weight As Integer ' Weight as an integer
+    Private name As String ' the Dogs name as text
+    Public Sub bark()
+        'code here to bark
+    End Sub
+
+End Class
