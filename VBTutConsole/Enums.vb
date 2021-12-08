fc348b265025f55dd09360b80450b81ec14e06dd
Marco Preciado <mac46005@gmail.com>
2021-12-07 23:04:47 -0600

    Enums in vb

---
+++ VBTutConsole/Enums.vb
@@ -0,0 +1,8 @@
+﻿Enum Tempatures
+    CelsiusMeetsFahrenheit = -40
+    WickedCold = 0
+    FreezingPoint = 32
+    LightJacketWeather = 60
+    SwimmingWeather = 72
+    BoilingPoint = 212
+End Enum
