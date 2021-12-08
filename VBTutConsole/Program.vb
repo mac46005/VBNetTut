eb5d87ffc54f10ee08d8b8040ccb45ac248411c8
Marco Preciado <mac46005@gmail.com>
2021-12-08 06:57:53 -0600

    Casting in Vb

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -1,10 +1,17 @@
+Option Strict On
 Imports System.Console
-
 Module Program
     Sub Main(args As String())
-        WriteLine($"Freezing Point of water: {Tempatures.FreezingPoint}")
-        WriteLine($"Boiling Point of water: {Tempatures.BoilingPoint}")
-
+        Dim castIn As Integer = CInt("5")
+        Dim castBool As Boolean = CBool(0)
+        Dim castByte As Byte = CByte("255")
+        Dim castChar As Char = CChar("The End")
+        Dim castDate As Date = CDate("11/12/2021")
+        Dim castLong As Long = CLng("5000000000")
+        Dim castObj As Object = CObj(Nothing)
+        Dim castShort As Short = CShort(0.33)
+        Dim castString As String = CStr(True)
+        Dim castType As Integer = CType(Tempatures.BoilingPoint, Integer)
     End Sub
 
 
