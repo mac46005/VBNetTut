e91f2adc1e05ba9ab069b4d8668f8db9d132ea22
Marco Preciado <mac46005@gmail.com>
2021-12-07 22:28:51 -0600

    R on integer represents a double value

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -2,7 +2,7 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim myValue As Integer = 32 'assign the literal value 32
+        Dim myValue As Integer = 32.0R 'The double value 32
     End Sub
 
 
