a96fe817ec566c50c58de6de40ca9258f3e25f76
Marco Preciado <mac46005@gmail.com>
2021-12-07 22:35:51 -0600

    Data Types

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -2,7 +2,30 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim myValue As Integer = 32.0R 'The double value 32
+        ' Boolean
+        Dim booleanValue As Boolean = True
+        ' Char
+        Dim charValue As Char = "J"c
+        ' Decimal
+        Dim decimalValue As Decimal = 3.141D
+        ' Double
+        Dim doubleValue As Double = 3.1415
+        Dim doubleval As Double = 3.1214R
+        Dim dobval As Double = 5.0R
+        ' Integer
+        Dim intVal As Integer = 100
+        Dim integerValue As Integer = 100I
+        ' Long
+        Dim longVal As Long = 5000000000000
+        Dim longValue As Long = 100L
+        ' Short
+        Dim shortValue As Short = 100S
+        ' Single
+        Dim singleValue As Single = 2.1F
+        ' String
+        Dim stringValue As String = "Hello World!"
+
+
     End Sub
 
 
