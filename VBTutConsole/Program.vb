ff4a3561692e73dfb18cebb66f4228eb5b758917
Marco Preciado <mac46005@gmail.com>
2021-12-08 07:27:40 -0600

    Intro to If in vb

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -2,10 +2,22 @@ Option Strict On
 Imports System.Console
 Module Program
     Sub Main(args As String())
-        ' Single line statement
-        WriteLine("This is a statement on a single line.")
-        ' Multiple statements on a single line
-        WriteLine("First statement.") : WriteLine("Second Statement")
-    End Sub
+        Dim valueOne As Integer = 10
+        Dim valueTwo As Integer = 20
+        Dim valueThree As Integer = 30
 
+        WriteLine("Testing valueOne against valueTwo...")
+        If valueOne > valueTwo Then
+            WriteLine($"{NameOf(valueOne)}: {valueOne} larger than {NameOf(valueTwo)}:{valueTwo}")
+        End If
+
+        WriteLine("Testing valueThree against valueTwo...")
+        If valueThree > valueTwo Then
+            WriteLine($"{NameOf(valueThree)}: {valueThree} larger than {NameOf(valueTwo)}: {valueTwo}")
+        End If
+
+        WriteLine("Testing is valueTwo > 15 on one line")
+        If valueTwo > 15 Then WriteLine("Yes it is")
+
+    End Sub
 End Module
