777a9bc5368d884f6236cf8f4a0805abc9f9344b
Marco Preciado <mac46005@gmail.com>
2021-12-08 21:56:39 -0600

    Else if VB

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -2,13 +2,18 @@ Option Strict On
 Imports System.Console
 Module Program
     Sub Main(args As String())
-        Dim temp As Integer = 32
+        Dim temp As Integer = -32
 
-        If temp <= 32 Then
-            WriteLine("Warning! Ice on the road!")
-            If temp = 32 Then
-                WriteLine("Temp exactly freezing, beware of water.")
-            End If
+        If temp > 32 Then
+            WriteLine("Safe driving...")
+        ElseIf temp = 32 Then
+            WriteLine("Warning, 32 degrees, watch for ice and water")
+        ElseIf temp > 0 Then
+            WriteLine("Watch for ice...")
+        ElseIf temp = 0 Then
+            WriteLine("Temperature = 0")
+        Else
+            WriteLine("Temperatures below zero, Wicked Cold!")
         End If
     End Sub
 End Module
