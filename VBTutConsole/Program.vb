afab0ead240076cbcac739353be2042fae3a3e76
Marco Preciado <mac46005@gmail.com>
2021-12-08 22:12:52 -0600

    Case using Is and To in VB

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -2,17 +2,17 @@ Option Strict On
 Imports System.Console
 Module Program
     Sub Main(args As String())
-        Dim targetInteger As Integer = 15
+        Dim targetInteger As Integer = 7
 
         Select Case targetInteger
-            Case 5
-                WriteLine("5")
-            Case 10
-                WriteLine("10")
+            Case Is < 10
+                WriteLine("Less than 10")
+            Case 10 To 14
+                WriteLine("10-14")
             Case 15
-                WriteLine("15")
+                WriteLine("15!")
             Case Else
-                WriteLine("Value not found")
+                WriteLine("Value not found!")
         End Select
     End Sub
 End Module
