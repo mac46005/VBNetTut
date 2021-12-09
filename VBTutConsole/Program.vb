c9f3eb09f0467215294d26d59a56a1de0b82ca69
Marco Preciado <mac46005@gmail.com>
2021-12-08 22:55:05 -0600

    Step in for loop to adjust increment in VB

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -4,7 +4,7 @@ Module Program
     Sub Main(args As String())
         Dim loopCounter As Single
 
-        For loopCounter = 0.5 To 9
+        For loopCounter = 0.5 To 9 Step 0.5
             WriteLine($"{NameOf(loopCounter)} : {loopCounter}")
         Next
     End Sub
