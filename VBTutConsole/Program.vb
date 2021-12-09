25b4b295aa8264f20f54606715bf79b6f9ce0758
Marco Preciado <mac46005@gmail.com>
2021-12-08 22:44:00 -0600

    Do Exit statements in VB

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -2,11 +2,15 @@ Option Strict On
 Imports System.Console
 Module Program
     Sub Main(args As String())
-        Dim counterVariable As Integer = 100
+        Dim counterVariable As Integer = 0
 
         Do
             WriteLine("counterVariable: {0}", counterVariable)
+
             counterVariable += 1
-        Loop While counterVariable < 10
+            If counterVariable > 9 Then
+                Exit Do
+            End If
+        Loop
     End Sub
 End Module
