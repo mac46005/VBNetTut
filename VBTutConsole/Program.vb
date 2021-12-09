4f41a7f8679e0f2edc8f254c24a8f4e0eaeb6933
Marco Preciado <mac46005@gmail.com>
2021-12-08 22:29:32 -0600

    the GoTo keyword is rarley used. But understood. VB

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -2,11 +2,14 @@ Option Strict On
 Imports System.Console
 Module Program
     Sub Main(args As String())
-        Dim target As String = "Milo"
+        Dim counterVariable As Integer = 0
 
-        Select Case target
-            Case "Fred", "Joe", Is < "Alpha"
-                WriteLine("Joe or Fres or < Aardzark")
-        End Select
+repeat: ' the label
+        WriteLine($"Counter variable: {counterVariable}")
+        ' Increment the counter
+        counterVariable += 1
+        If counterVariable < 10 Then
+            GoTo repeat ' the dastardly deed
+        End If
     End Sub
 End Module
