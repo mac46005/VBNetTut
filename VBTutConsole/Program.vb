1937e26dabb96322f919c05bfedcf2c8f2ba817b
Marco Preciado <mac46005@gmail.com>
2021-12-11 19:57:15 -0600

    Basic Relational Operators in vb

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -3,10 +3,15 @@ Option Strict Off
 Imports System.Console
 Module Program
     Sub Main(args As String())
-        Dim value As Integer = 5
-        Dim power As Integer = 4
-
-        WriteLine($"{value} to the {power}th power is {value ^ power}")
+        Dim valueOne As Integer = 4
+        Dim valueTwo As Integer = 5
+        WriteLine($"{valueOne} = {valueTwo} => {valueOne = valueTwo}")
+        ' NOT EQUAL
+        WriteLine($"{valueOne} <> {valueTwo} => {valueOne <> valueTwo}")
+        WriteLine($"{valueOne} > {valueTwo} => {valueOne = valueTwo}")
+        WriteLine($"{valueOne} => {valueTwo} => {valueOne >= valueTwo}")
+        WriteLine($"{valueOne} >= {valueTwo} => {valueOne >= valueTwo}")
+        WriteLine($"{valueOne} =< {valueTwo} => {valueOne <= valueTwo}")
         ReadLine()
     End Sub
 End Module
