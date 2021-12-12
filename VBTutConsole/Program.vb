58fb781517fd02736baf86769cd028971214f045
Marco Preciado <mac46005@gmail.com>
2021-12-11 19:37:51 -0600

    Exponentiation Operator in vb

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -3,17 +3,10 @@ Option Strict Off
 Imports System.Console
 Module Program
     Sub Main(args As String())
-        Dim counter As Integer
+        Dim value As Integer = 5
+        Dim power As Integer = 4
 
-        'count from 1 to 100
-        For counter = 1 To 100
-            ' display the value
-            Write($"{counter} ")
-
-            ' every tenth value, display a tab and the value
-            If counter Mod 10 = 0 Then
-                WriteLine(vbTab & counter)
-            End If
-        Next counter
+        WriteLine($"{value} to the {power}th power is {value ^ power}")
+        ReadLine()
     End Sub
 End Module
