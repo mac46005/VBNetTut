46239a3f69dfa4fa7b4e387ffb69f3b644c3c1f1
Marco Preciado <mac46005@gmail.com>
2021-12-12 19:58:05 -0600

    data types create copies class types only reference one on memory heap

---
+++ VBTutConsole/DogModel.vb
@@ -0,0 +1,8 @@
+﻿Public Class DogModel
+    Dim age As Integer ' the dog's age
+    Dim weight As Integer ' th dog's weight
+
+    Public Sub Bark()
+        System.Console.WriteLine("The dog barks!")
+    End Sub
+End Class
--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -1,32 +1,49 @@
 Option Strict Off
 Imports System.Console
 Module Program
-    Sub Main(args As String())
-        Dim minDrinkingCoffee As Integer = 5
-        Dim minReadingNewspaper As Integer = 10
-        Dim minArguing As Integer = 15
-        Dim minDawdling As Integer = 20
 
-        Dim numAdults As Integer = 2
-        Dim numChildren As Integer = 2
+    Public Class Bitch
+        Public Weight As Integer
+    End Class
+    Public Class Tester
+        Public Shared Sub Main()
+            Dim testobject As New Tester()
+            testobject.Run()
 
-        Dim wastebByEachAdult As Integer
-        Dim wastedByAllAdults As Integer
-        Dim wastedByEachKid As Integer
-        Dim wastedByAllKids As Integer
-        Dim wastedByFamily As Integer
-        Dim totalSeconds As Integer
+        End Sub
+        Public Sub Run()
+            'created an integer
+            Dim firstInt As Integer = 5
 
-        wastebByEachAdult = minDrinkingCoffee + minReadingNewspaper
-        wastedByAllAdults = wastebByEachAdult * numAdults
-        wastedByEachKid = wastedByEachKid * numChildren
-        wastedByFamily = wastedByAllAdults + wastedByAllKids
-        totalSeconds = wastedByFamily * 60
+            ' create a second integer
+            Dim secondInt As Integer = firstInt
 
-        WriteLine($"Each adult wastes {wastebByEachAdult} minutes")
-        WriteLine($"Each child wasted by entire family: {wastedByAllKids}")
-        WriteLine($"Total wasted seconds: {totalSeconds}")
+            ' display the two integers
+            Console.WriteLine($"firstInt: {firstInt}, secondInt: {secondInt}")
+
+            ' modify the second integer 
+            secondInt = 7
+
+            ' display the two integers
+            Console.WriteLine($"firstInt: {firstInt}, secondInt: {secondInt}")
+
+            ' create dog
+            Dim bitch As New Bitch()
+
+            ' assign a value to weight
+            bitch.Weight = 3
+
+            ' create a second reerence to the bitch
+            Dim bitchTwo As Bitch = bitch
+
+            ' display thier values
+            Console.WriteLine($"bitch: {bitch.Weight}, bitchTwo: {bitchTwo.Weight}")
+            bitchTwo.Weight = 7
+
+            ' display the two values
+            Console.WriteLine($"bitch: {bitch.Weight}, bitchTwo: {bitchTwo.Weight}")
 
 
-    End Sub
+        End Sub
+    End Class
 End Module
