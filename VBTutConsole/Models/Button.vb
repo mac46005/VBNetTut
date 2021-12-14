4ef08f6814ba0e9936c0e88323f15169fecd36a7
Marco Preciado <mac46005@gmail.com>
2021-12-14 16:09:49 -0600

    Line manipulator in use ControlChars.Lf ewww....

---
+++ VBTutConsole/Models/Button.vb
@@ -0,0 +1,13 @@
+﻿Public Class Button
+    Inherits Window
+
+    Public Sub New(top As Integer, left As Integer)
+        MyBase.New(top, left)
+    End Sub
+
+    ' an ovverridden version (note keyword) because in the
+    ' derived method we change the behavior
+    Public Overrides Sub DrawingWindow()
+        Console.WriteLine($"Drawing a button at ({_left},{_top})      {ControlChars.Lf}")
+    End Sub
+End Class
