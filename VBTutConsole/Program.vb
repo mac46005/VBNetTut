7bc0d9de2edcbe52df6922abfc4f1bd0440ea884
Marco Preciado <mac46005@gmail.com>
2021-12-14 13:25:09 -0600

    Demo work on a property of an object of the type TimeProps

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -3,11 +3,26 @@ Imports System.Console
 Module Program
     Public Class Tester
         Public Shared Sub Main()
-            Dim currentTime As System.DateTime = System.DateTime.Now
-            Dim time1 As New Time(currentTime)
+            Dim currentTime As System.DateTime = DateTime.Now
+            Dim time1 As New TimeProps(currentTime)
             time1.DisplayCurrentTime()
-            Dim time2 As New Time(2021, 12, 19, 3, 45, 9)
-            time2.DisplayCurrentTime()
+
+            'extract the hour to a local variable
+            Dim theHour As Integer = time1.Hour
+
+            ' display the local variable
+            Console.WriteLine($"Retrieved the hour: {theHour}")
+
+            ' add one to the local variable
+            theHour += 1
+
+            ' write the time back to the object
+            time1.Hour = theHour
+
+
+            'display the result 
+            Console.WriteLine($"Updated the hour: {time1.Hour}")
+
         End Sub
     End Class
 
