c3242d490918a4bb8d6ffced3b3b5648297c0ee5
Marco Preciado <mac46005@gmail.com>
2021-12-14 09:32:23 -0600

    Added form to proj

---
+++ VBTutConsole/TestUsingDispose.vb
@@ -0,0 +1,27 @@
+﻿Public Class TestUsingDispose
+    Implements IDisposable
+    Dim is_disposed As Boolean = False
+    Protected Sub Dispose(ByVal disposing As Boolean)
+        If Not is_disposed Then
+            If disposing Then
+                Console.WriteLine("Not in destructor, OK to reference other objects")
+            End If
+            ' perform cleanup for this object
+            Console.WriteLine("Disposing...")
+
+        End If
+        Me.is_disposed = True
+    End Sub
+
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        Dispose(True)
+        ' tell the GC not to finalize
+        GC.SuppressFinalize(Me)
+    End Sub
+
+    Protected Overrides Sub Finalize()
+        Dispose(False)
+        Console.WriteLine("In destructor")
+    End Sub
+End Class
