51544bd8ed81e16b5163209d04317e0ed3a675e6
Marco Preciado <mac46005@gmail.com>
2021-12-14 13:49:13 -0600

    Testing ByVal output in demo methods in vb

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -3,27 +3,41 @@ Imports System.Console
 Module Program
     Public Class Tester
         Public Shared Sub Main()
-            Dim currentTime As System.DateTime = DateTime.Now
-            Dim time1 As New TimeProps(currentTime)
-            time1.DisplayCurrentTime()
+            Dim test As New Tester()
+            test.Run()
 
-            'extract the hour to a local variable
-            Dim theHour As Integer = time1.Hour
-
-            ' display the local variable
-            Console.WriteLine($"Retrieved the hour: {theHour}")
-
-            ' add one to the local variable
-            theHour += 1
-
-            ' write the time back to the object
-            time1.Hour = theHour
-
-
-            'display the result 
-            Console.WriteLine($"Updated the hour: {time1.Hour}")
 
         End Sub
+        Public Sub Run()
+            ' declare a variable and initialize to 5
+            Dim theVariable As Integer = 5
+
+            ' display its value
+            Console.WriteLine($"In Run. theVariable: {theVariable}")
+            ' call the method and pass in the variable
+            Doubler(theVariable)
+            ' return and display the value again
+            Console.WriteLine($"Back in run. theVariable: {theVariable}")
+            EndOfSubFunc(NameOf(Run))
+
+        End Sub
+
+        Public Sub Doubler(ByVal param As Integer)
+            ' display the value that was passed in
+            Console.WriteLine($"In Double(). Revecieved param: {param}")
+
+            ' double the value
+            param *= 2
+
+            ' Display the doubled value before returning
+            Console.WriteLine($"updated param. Return new value: {param}")
+            EndOfSubFunc(NameOf(Doubler))
+        End Sub
+        Public Sub EndOfSubFunc(Inside As String)
+            Console.WriteLine($"End of {Inside}")
+        End Sub
+
+
     End Class
 
 End Module
