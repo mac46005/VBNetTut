2e11cf7fc268b3c2f75e7c048dda923cfe9d5ac7
Marco Preciado <mac46005@gmail.com>
2021-12-15 11:58:50 -0600

    structure in vb

--- VBTutConsole/Structures/StructureDemonstration.vb
+++ VBTutConsole/Structures/StructureDemonstration.vb
@@ -1,5 +1,34 @@
 ﻿Namespace My.Structures
     Public Structure Location
+        Private myXVal As Integer
+        Private myYVal As Integer
+        ' constructor
+        Public Sub New(xCoordinate As Integer, yCoordinate As Integer)
+            myXVal = xCoordinate
+            myYVal = yCoordinate
+        End Sub
 
+        ' property
+        Public Property XVal() As Integer
+            Get
+                Return myXVal
+            End Get
+            Set(value As Integer)
+                myXVal = value
+            End Set
+        End Property
+
+        Public Property YVal() As Integer
+            Get
+                Return myYVal
+            End Get
+            Set(value As Integer)
+                myYVal = value
+            End Set
+        End Property
+        ' display the structure as a string
+        Public Overrides Function ToString() As String
+            Return [String].Format("{0},{1}", XVal, YVal)
+        End Function
     End Structure
 End Namespace
