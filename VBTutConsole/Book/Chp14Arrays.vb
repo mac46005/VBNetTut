b929245639dd3a3758a2cf838b85df1d11a1f2f8
Marco Preciado <mac46005@gmail.com>
2021-12-16 13:05:48 -0600

    Adding values to arrays on initianlizing

--- VBTutConsole/Book/Chp14Arrays.vb
+++ VBTutConsole/Book/Chp14Arrays.vb
@@ -1,5 +1,11 @@
 ﻿Namespace Chp14Arrays
     Public Class Employee
+        ' adding predefined values in an array
+        Dim myIntArray1() As Integer = {2, 3, 4, 5, 6, 7}
+        Dim myIntArray2() As Integer = New Integer(4) {2, 3, 4, 5, 6}
+
+
+
         Private _empId As Integer
         ' constructor
         Public Sub New(empId As Integer)
