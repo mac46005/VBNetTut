9089ff6c56597bad34d6813f18481adcfd12d660
Marco Preciado <mac46005@gmail.com>
2021-12-16 12:07:37 -0600

    Adding initial size to array

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -5,8 +5,10 @@ Imports VBTutConsole.OverridingInterfaces
 Module Program
     Public Class Tester
         Public Shared Sub Main()
-            Dim myIntArrayImplicit() As Integer ' implicit constructor
-            Dim myIntArrayExplicit As Integer = New Integer() 'explicit constructor
+            Dim myIntArrayImplicit(7) As Integer ' implicit contrsuctor, 7 members
+            Dim myIntArrayExplicit As Integer() = New Integer(7) {} ' I think the author made a mistake
+            ' Dim myIntArray as Integer = New Integer(7){} <= this is what he got. I keep getting an error though....
+
         End Sub
     End Class
 End Module
