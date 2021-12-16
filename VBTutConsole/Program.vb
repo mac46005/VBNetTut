37fd12f7ec7ff5248a14dee1e183ee4ddcb73cac
Marco Preciado <mac46005@gmail.com>
2021-12-16 13:18:47 -0600

    Explicitly dumping values into a method params as ParamArray

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -10,6 +10,12 @@ Module Program
             Chp14Arrays.ArrayExamples.DisplayVals(3, 4, 5, 3, 5, 6)
 
 
+            ' In this example I explicitly created an array of numbers and
+            ' dumped in the DisplayVals parameters
+            Dim explicitArray() As Integer = {3, 4, 5, 6, 7, 8}
+            Chp14Arrays.ArrayExamples.DisplayVals(explicitArray)
+
+
         End Sub
     End Class
 End Module
