97a516a769042720f476e68557881a781cb6f8d8
Marco Preciado <mac46005@gmail.com>
2021-12-16 12:15:04 -0600

    Redim and ReDim Preserver keywords for resizizing ARRAYS and keeping data if need to.

--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -9,6 +9,20 @@ Module Program
             Dim myIntArrayExplicit As Integer() = New Integer(7) {} ' I think the author made a mistake
             ' Dim myIntArray as Integer = New Integer(7){} <= this is what he got. I keep getting an error though....
 
+
+            ' Resizing an array
+            Dim myArrayString As String() = New String(4) {}
+
+            myArrayString(0) = "valueone"
+            myArrayString(1) = "valuetwo"
+            myArrayString(2) = "valuethree"
+            myArrayString(3) = "valuefour"
+
+            ' You can resize an array from its current size using ReDim
+            ReDim myArrayString(50)
+
+            ' You can make the same change to array, but preserve the existing data in the array by writing:
+            ReDim Preserve myArrayString(50)
         End Sub
     End Class
 End Module
