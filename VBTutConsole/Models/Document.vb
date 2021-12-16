1e8bf39a6581365dbb876b2b4d41f746267f41c3
Marco Preciado <mac46005@gmail.com>
2021-12-16 10:26:05 -0600

    Interface interactions

--- VBTutConsole/Models/Document.vb
+++ VBTutConsole/Models/Document.vb
@@ -1,20 +1,34 @@
 ﻿Public Class Document
-    Implements IStorable
-
+    Implements IStorable, ICompressible
+    Private _status As Integer
     Public Property Status As Integer Implements IStorable.Status
         Get
-            Throw New NotImplementedException()
+            Return _status
         End Get
         Set(value As Integer)
-            Throw New NotImplementedException()
+            _status = value
         End Set
     End Property
 
-    Public Sub Read() Implements IStorable.Read
-        Throw New NotImplementedException()
+    ' constructor
+    Public Sub New(s As String)
+        Console.WriteLine($"Creating document with: {s}")
     End Sub
 
-    Public Sub Write() Implements IStorable.Write
-        Throw New NotImplementedException()
+
+    Public Sub Read() Implements IStorable.Read
+        Console.WriteLine("Document.Reading()")
+    End Sub
+
+    Public Sub Write(obj As Object) Implements IStorable.Write
+        Console.WriteLine("Document.Reading()")
+    End Sub
+
+    Public Sub Compress() Implements ICompressible.Compress
+        Console.WriteLine("Document.Compress()")
+    End Sub
+
+    Public Sub Decompress() Implements ICompressible.Decompress\
+        Console.WriteLine("Document.Decompress()")
     End Sub
 End Class
--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -5,25 +5,17 @@ Imports VBTutConsole.My.Structures
 Module Program
     Public Class Tester
         Public Shared Sub Main()
-            ' create an instance of the structure
-            Dim loc1 As New Location(200, 300)
+            Dim doc As New Document("Test Document")
+            doc.Read()
+            doc.Status = -1
+            ' the power of interfaces. Never forget.
+            Dim isDoc As IStorable = doc
+            isDoc.Status = 0
+            isDoc.Read()
 
-            ' display the values in the structure
-            Console.WriteLine($"Loc1 location:{loc1}")
 
-            ' invoke the default constructor
-            Dim loc2 As New Location()
-            Console.WriteLine($"Loc2 location: {loc2}")
 
-            ' pass the constructor to a method
-            myFunc(loc1)
-            Console.WriteLine($"Loc1 location: {loc1}")
-        End Sub
-        Public Shared Sub myFunc(loc As Location)
-            ' modify the values through the properties
-            loc.XVal = 49
-            loc.YVal = 300
-            Console.WriteLine($"{loc} location: {loc}")
+
         End Sub
     End Class
 End Module
