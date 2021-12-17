4855190bdcc962c3bbb0a890739bcb7468906e72
Marco Preciado <mac46005@gmail.com>
2021-12-17 10:53:16 -0600

    Continue on operations

--- VBTutConsole/Book/Chapter15.vb
+++ VBTutConsole/Book/Chapter15.vb
@@ -29,6 +29,58 @@
                 End If
             End Function
 
-        End Class
+            Public Sub Reset() Implements IEnumerator.Reset
+                index = -1
+            End Sub
+
+
+            ' current property defined as the
+            ' last string added to the listbox
+            Public ReadOnly Property Current() As Object Implements IEnumerator.Current
+                Get
+                    Return currentListBox(index)
+                End Get
+            End Property
+        End Class ' end nested class
+
+        ' Enumerable classes can return an enumerator
+        Public Function GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+            Return New ListBoxEnumerator(Me)
+        End Function
+
+
+        ' initialize the list box with strings
+        Public Sub New(ParamArray initialStrings() As String)
+            ' allocate space for strings
+            ReDim strings(7)
+
+            ' copy the strings passed in to the constructor
+            For Each s As String In initialStrings
+                strings(ctr) = s
+                ctr += 1
+            Next
+        End Sub
+
+
+        ' allow array-like access
+        Default Public Property Item(index As Integer) As String
+            Get
+                If index < 0 Or index >= strings.Length Then
+                    ' handle bad index
+                    Exit Property
+                End If
+                Return strings(index)
+            End Get
+            Set(value As String)
+                strings(index) = value
+            End Set
+        End Property
+
+        ' publish how many strings you hold
+        Public Function GetNumEntries() As Integer
+            Return ctr
+        End Function
     End Class
+
+
 End Namespace
--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -5,9 +5,6 @@ Imports VBTutConsole.Chp14Arrays
 Module Program
     Public Class Tester
         Public Shared Sub Main()
-            Dim tester As Chapter15.Tester
-            tester = New Chapter15.Tester()
-            tester.Run()
         End Sub
     End Class
 End Module
