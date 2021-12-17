f7336b4c089c2e2092930a2651d5c62e20cec58b
Marco Preciado <mac46005@gmail.com>
2021-12-17 11:38:02 -0600

    Queues in vb

--- VBTutConsole/Book/Chapter15.vb
+++ VBTutConsole/Book/Chapter15.vb
@@ -82,5 +82,165 @@
         End Function
     End Class
 
+    ' a class o hold in the array list
+    Public Class Employee
+        Private myEmpId As Integer
+
+        Public Sub New(empId As Integer)
+            Me.myEmpId = empId
+        End Sub ' new 
+
+        Public Overrides Function ToString() As String
+            Return myEmpId.ToString()
+        End Function
+
+        Public Property EmpId() As Integer
+            Get
+                Return myEmpId
+            End Get
+            Set(value As Integer)
+                myEmpId = value
+            End Set
+        End Property
+    End Class
+
+    Public Class Tester
+        Public Sub Run()
+            Dim empArray As New ArrayList()
+            Dim intArray As New ArrayList()
+
+            ' populate the arrayLists
+            For i = 0 To 4
+                empArray.Add(New Employee(i + 100))
+                intArray.Add(i * 5)
+            Next
+
+            ' print each member of the array
+            For Each i In intArray
+                Console.WriteLine($"{i}")
+            Next
+
+            Console.WriteLine(ControlChars.Lf)
+
+
+            ' print each employee
+            For Each e As Employee In empArray
+                Console.WriteLine($"{e.ToString()}")
+            Next
+
+            Console.WriteLine(ControlChars.Lf)
+
+            Console.WriteLine($"empArray.Capacity: {empArray.Capacity}")
+        End Sub
+    End Class
+    Public Class CollectionTester
+        Public Sub Run()
+            Dim intCollection As New Collection()
+            Dim empCollection As New Collection()
+            Dim empCollection2 As New Collection()
+
+            ' populate the collections
+            Dim i As Integer
+            For i = 0 To 4
+                empCollection.Add(New Employee(i + 100))
+                intCollection.Add(i * 500)
+            Next
+
+            ' add key/value pairs
+            empCollection2.Add(New Employee(1789), "George Washington")
+            empCollection2.Add(New Employee(1797), "John Adams")
+            empCollection2.Add(New Employee(1801), "Thomas Jefferson")
+
+
+
+            ' print each member of the array
+            For Each int As Integer In intCollection
+                Console.WriteLine(int.ToString())
+            Next
+
+            Console.WriteLine()
+            Console.WriteLine("Employee collection...")
+            For Each e As Employee In empCollection
+                Console.WriteLine(e)
+            Next
+
+            Console.WriteLine()
+            Console.WriteLine("Employee Collection 2...")
+            For Each e As Employee In empCollection2
+                Console.WriteLine(e)
+            Next
+
+
+
+            Console.WriteLine()
+
+            ' retrieve an employee by key
+            Dim emp As Employee
+            emp = empCollection2.Item("John Adams")
+
+            Console.WriteLine("Key John Adas retrieved empId {0}", emp)
+
+            ' note that indexing is 1-based (rather than zero based)
+            emp = empCollection2.Item(1)
+            Console.WriteLine("Index(1) retreived empId {0}", emp)
+        End Sub
+    End Class
+
+
+    Public Class QueueTester
+        Public Sub Run()
+            Dim intQueue As New Queue()
+            ' populate the array
+            Dim i As Integer
+            For i = 0 To 4
+                intQueue.Enqueue(i * 5)
+            Next
+
+
+            ' display the queue
+            Console.WriteLine("intQueue values:")
+            DisplayValue(intQueue)
+
+            ' remove an element from the queue
+            Console.WriteLine($"(Dequeue) {intQueue.Dequeue()}")
+
+            ' display the queue
+            Console.WriteLine("intQueue values:")
+            DisplayValue(intQueue)
+
+
+            ' Remove another element from the queue
+            Console.WriteLine($"(Dequeue) {intQueue.Dequeue()}")
+
+
+            ' diplay the queue
+            Console.WriteLine($"intQueue values:")
+            DisplayValue(intQueue)
+
+            ' View the first element in the
+            ' queue but do not remove
+            Console.WriteLine($"(peek) {intQueue.Peek()}")
+
+            ' display the queue
+            Console.WriteLine("IntQueue value:")
+            DisplayValue(intQueue)
+
+
+
+
+
+        End Sub
+
+        Public Shared Sub DisplayValue(myCollection As IEnumerable)
+            Dim myEnumerator As IEnumerator = myCollection.GetEnumerator()
+
+            While myEnumerator.MoveNext()
+                Console.WriteLine($"{myEnumerator.Current}")
+            End While
+            Console.WriteLine()
+        End Sub ' DisplayValues
+
+
+    End Class
 
 End Namespace
--- VBTutConsole/Program.vb
+++ VBTutConsole/Program.vb
@@ -5,6 +5,9 @@ Imports VBTutConsole.Chp14Arrays
 Module Program
     Public Class Tester
         Public Shared Sub Main()
+            Dim tester As Chapter15.QueueTester
+            tester = New Chapter15.QueueTester()
+            tester.Run()
         End Sub
     End Class
 End Module
